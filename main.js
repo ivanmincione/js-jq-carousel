0054f7b581a355e6af8632e18857d6a39a0bc6d7
Ivan Mincione <ivan.mincione92@outlook.it>
2020-10-27 17:13:18 +0100

    Create due funzioni esterne e richiamate

--- main.js
+++ main.js
@@ -4,44 +4,53 @@ $(document).ready(function() {
 
     // click sull'icona arrow-right
     $(".arrow-right i").click(function() {
-        // variabile per salvare l'immagine con class active
-        //  variabile per salvare il cerchio con classe active
-        var circleActive = $(".circle i.active");
-        var imgActive  = $("img.active");
-        // rimozione della class active sull'immagine e sul circle per non sovrapporle
-        imgActive.removeClass("active");
-        circleActive.removeClass("active");
-        // è presente più di un immagine??
-        if (imgActive.next("img").length == 1) {
-            imgActive.next("img").addClass("active");
-            circleActive.next("i").addClass("active");
-        } else {
-            $(".slides :first-child").addClass("active");
-            $(".circle :first-child").addClass("active");
-
-        }
+        arrowRightClick();
     });
 
-
-    // procedimento uguale per il click sull'icona arrow-left modificando con prev e last-of-type.
-
+    // click sull'icona arrow-left
     $(".arrow-left i").click(function() {
-        // variabile per salvare l'immagine con class active
-        var imgActive  = $("img.active");
-        //  variabile per salvare il cerchio con classe active
-        var circleActive = $(".circle i.active");
-        // rimozione della class active sull'immagine e sul circle per non sovrapporle
-        imgActive.removeClass("active");
-        circleActive.removeClass("active");
-        // è presente più di un immagine??
-        if (imgActive.prev("img").length == 1) {
-            imgActive.prev("img").addClass("active");
-            circleActive.prev("i").addClass("active");
-        } else {
-            $(".slides img:last-of-type").addClass("active");
-            $(".circle i:last-of-type").addClass("active");
-
-        }
+        arrowLeftClick();
     });
+
 
 });
+
+
+// ----------------Funzioni----------------
+
+function arrowRightClick(){
+    // variabile per salvare l'immagine con class active
+    //  variabile per salvare il cerchio con classe active
+    var imgActive  = $("img.active");
+    var circleActive = $(".circle i.active");
+    // rimozione della class active sull'immagine e sul circle per non sovrapporle
+    imgActive.removeClass("active");
+    circleActive.removeClass("active");
+    // è presente più di un immagine??
+    if (imgActive.next("img").length == 1) {
+        imgActive.next("img").addClass("active");
+        circleActive.next("i").addClass("active");
+    } else {
+        $(".slides :first-child").addClass("active");
+        $(".circle :first-child").addClass("active");
+    }
+}
+
+
+function arrowLeftClick() {
+    // variabile per salvare l'immagine con class active
+    //  variabile per salvare il cerchio con classe active
+    var imgActive  = $("img.active");
+    var circleActive = $(".circle i.active");
+    // rimozione della class active sull'immagine e sul circle per non sovrapporle
+    imgActive.removeClass("active");
+    circleActive.removeClass("active");
+    // è presente più di un immagine??
+    if (imgActive.prev("img").length == 1) {
+        imgActive.prev("img").addClass("active");
+        circleActive.prev("i").addClass("active");
+    } else {
+        $(".slides img:last-of-type").addClass("active");
+        $(".circle i:last-of-type").addClass("active");
+    }
+}
