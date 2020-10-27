93260158740eb19017c6cc11a4f618133d90582e
Ivan Mincione <ivan.mincione92@outlook.it>
2020-10-27 16:57:38 +0100

    Aggiunti circle. Inizio bonus
    Facendo click sulle freccie scorrono le immagini ed i pallini sia a dx sia a sx.

--- main.js
+++ main.js
@@ -5,15 +5,20 @@ $(document).ready(function() {
     // click sull'icona arrow-right
     $(".arrow-right i").click(function() {
         // variabile per salvare l'immagine con class active
+        //  variabile per salvare il cerchio con classe active
+        var circleActive = $(".circle i.active");
         var imgActive  = $("img.active");
-        // rimozione della class active sull'immagine
+        // rimozione della class active sull'immagine e sul circle per non sovrapporle
         imgActive.removeClass("active");
+        circleActive.removeClass("active");
         // è presente più di un immagine??
         if (imgActive.next("img").length == 1) {
             imgActive.next("img").addClass("active");
-
+            circleActive.next("i").addClass("active");
         } else {
             $(".slides :first-child").addClass("active");
+            $(".circle :first-child").addClass("active");
+
         }
     });
 
@@ -23,18 +28,20 @@ $(document).ready(function() {
     $(".arrow-left i").click(function() {
         // variabile per salvare l'immagine con class active
         var imgActive  = $("img.active");
-        // rimozione della class active sull'immagine
+        //  variabile per salvare il cerchio con classe active
+        var circleActive = $(".circle i.active");
+        // rimozione della class active sull'immagine e sul circle per non sovrapporle
         imgActive.removeClass("active");
+        circleActive.removeClass("active");
         // è presente più di un immagine??
         if (imgActive.prev("img").length == 1) {
             imgActive.prev("img").addClass("active");
+            circleActive.prev("i").addClass("active");
         } else {
             $(".slides img:last-of-type").addClass("active");
+            $(".circle i:last-of-type").addClass("active");
+
         }
     });
-
-
-
-
 
 });
